74d977b35b861d87db42cb9c3073831f3053c2b0
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2022-12-23 19:21:29 +0300

    Update User_200709006.cpp

--- User_200709006.cpp
+++ User_200709006.cpp
@@ -1,12 +1,4 @@
-/*1-Introduction
--Affan Selim	
--KAYA
--200709006
--Computer Engineering (English)
--Object Oriented Programming--CEN-104-50-50
--2020/2021 2nd Somestr
--Project 2
--Asst.Prof.Ahmet COÞKUNÇAY */
+
 
 #include <string>
 #include "User_200709006.h"
