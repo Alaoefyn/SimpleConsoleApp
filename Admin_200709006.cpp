031278f234778ccf88034ca8180cdea156c7a695
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2022-12-23 19:19:34 +0300

    Update Admin_200709006.cpp

--- Admin_200709006.cpp
+++ Admin_200709006.cpp
@@ -1,12 +1,3 @@
-/*1-Introduction
--Affan Selim	
--KAYA
--200709006
--Computer Engineering (English)
--Object Oriented Programming--CEN-104-50-50
--2020/2021 2nd Somestr
--Project 2
--Asst.Prof.Ahmet COÞKUNÇAY */ 
 
 #include "Admin_200709006.h"
 
